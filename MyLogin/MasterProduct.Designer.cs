f3bd017432533f9293f82f11aeb707b2a1f094dc
Agil <Agil@LAPTOP-M8GGGF4A>
2021-05-25 15:24:28 +0700

    Add project files.

---
+++ MyLogin/MasterProduct.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace MyLogin
+{
+    partial class MasterProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSearchItem = new System.Windows.Forms.TextBox();
+            this.txtSearchDesign = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtDesign = new System.Windows.Forms.TextBox();
+            this.dateExpired = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dt = new System.Windows.Forms.DataGridView();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MASTER PRODUCT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Item Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Design";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Item Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Design";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Color";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 418);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Expired Date";
+            // 
+            // txtSearchItem
+            // 
+            this.txtSearchItem.Location = new System.Drawing.Point(164, 95);
+            this.txtSearchItem.Name = "txtSearchItem";
+            this.txtSearchItem.Size = new System.Drawing.Size(220, 22);
+            this.txtSearchItem.TabIndex = 8;
+            // 
+            // txtSearchDesign
+            // 
+            this.txtSearchDesign.Location = new System.Drawing.Point(164, 139);
+            this.txtSearchDesign.Name = "txtSearchDesign";
+            this.txtSearchDesign.Size = new System.Drawing.Size(197, 22);
+            this.txtSearchDesign.TabIndex = 9;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(164, 216);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(220, 22);
+            this.txtID.TabIndex = 10;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(164, 266);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(220, 22);
+            this.txtItem.TabIndex = 11;
+            // 
+            // txtDesign
+            // 
+            this.txtDesign.Location = new System.Drawing.Point(164, 318);
+            this.txtDesign.Name = "txtDesign";
+            this.txtDesign.Size = new System.Drawing.Size(197, 22);
+            this.txtDesign.TabIndex = 12;
+            // 
+            // dateExpired
+            // 
+            this.dateExpired.Location = new System.Drawing.Point(164, 418);
+            this.dateExpired.Name = "dateExpired";
+            this.dateExpired.Size = new System.Drawing.Size(255, 22);
+            this.dateExpired.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 481);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(226, 481);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 25);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(344, 481);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 25);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dt
+            // 
+            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt.Location = new System.Drawing.Point(46, 559);
+            this.dt.Name = "dt";
+            this.dt.RowTemplate.Height = 24;
+            this.dt.Size = new System.Drawing.Size(492, 183);
+            this.dt.TabIndex = 19;
+            // 
+            // cbColor
+            // 
+            this.cbColor.FormattingEnabled = true;
+            this.cbColor.Items.AddRange(new object[] {
+            "Red",
+            "Blue",
+            "White",
+            "Green",
+            "Yellow",
+            "Black"});
+            this.cbColor.Location = new System.Drawing.Point(164, 368);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(197, 24);
+            this.cbColor.TabIndex = 20;
+            // 
+            // MasterProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 763);
+            this.Controls.Add(this.cbColor);
+            this.Controls.Add(this.dt);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateExpired);
+            this.Controls.Add(this.txtDesign);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtSearchDesign);
+            this.Controls.Add(this.txtSearchItem);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MasterProduct";
+            this.Text = "Master Product";
+            this.Load += new System.EventHandler(this.MasterProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSearchItem;
+        private System.Windows.Forms.TextBox txtSearchDesign;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtDesign;
+        private System.Windows.Forms.DateTimePicker dateExpired;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dt;
+        private System.Windows.Forms.ComboBox cbColor;
+    }
+}
